4e88f4ee8ccc436beaf4e5afc120b949d60cfbb1
aimedidierm <aimedidiermugisha@gmail.com>
2023-10-18 19:23:26 +0200

    Configuring Tailwind CSS

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+
+<body class="bg-gray-100 flex items-center justify-center h-screen">
+    <div class="bg-white p-8 rounded shadow-lg w-96">
+        <h1 class="text-2xl font-semibold text-gray-800 mb-6">Login</h1>
+        <form>
+            <div class="mb-4">
+                <label for="email" class="block text-gray-600 text-sm font-medium mb-2">Email</label>
+                <input type="email" id="email" name="email"
+                    class="w-full border rounded-md py-2 px-3 text-gray-700 focus:outline-none focus:border-blue-400"
+                    placeholder="example@example.com" required>
+            </div>
+            <div class="mb-4">
+                <label for="password" class="block text-gray-600 text-sm font-medium mb-2">Password</label>
+                <input type="password" id="password" name="password"
+                    class="w-full border rounded-md py-2 px-3 text-gray-700 focus:outline-none focus:border-blue-400"
+                    placeholder="Password" required>
+            </div>
+            <button type="submit"
+                class="w-full bg-blue-500 text-white font-semibold rounded-md py-2 px-4 hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300">Login</button>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ resources/views/employee/attende.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+@section('content')
+<x-admin-nav />
+
+@stop
