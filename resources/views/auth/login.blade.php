8f24a8254f2c5d1d3ae5bff943682b728a578b61
aimedidierm <aimedidiermugisha@gmail.com>
2023-10-29 00:47:53 +0200

    Aunthentication

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,23 +11,26 @@
 <body class="bg-gray-100 flex items-center justify-center h-screen">
     <div class="bg-white p-8 rounded shadow-lg w-96">
         <h1 class="text-2xl font-semibold text-gray-800 mb-6">Login</h1>
-        {{-- <form method="POST"> --}}
+        <form method="POST" action="/login">
+            @csrf
+            @if($errors->any())<span
+                class="self-center text-1xl text-red-600 font-semibold whitespace-nowrap dark:text-white">{{$errors->first()}}</span>@endif
             <div class="mb-4">
                 <label for="email" class="block text-gray-600 text-sm font-medium mb-2">Email</label>
                 <input type="email" id="email" name="email"
                     class="w-full border rounded-md py-2 px-3 text-gray-700 focus:outline-none focus:border-blue-400"
-                    placeholder="example@example.com" required>
+                    placeholder="Enter Email" required>
             </div>
             <div class="mb-4">
                 <label for="password" class="block text-gray-600 text-sm font-medium mb-2">Password</label>
                 <input type="password" id="password" name="password"
                     class="w-full border rounded-md py-2 px-3 text-gray-700 focus:outline-none focus:border-blue-400"
-                    placeholder="Password" required>
+                    placeholder="Enter Password" required>
             </div>
             <button type="submit"
                 class="w-full bg-blue-500 text-white font-semibold rounded-md py-2 px-4 hover:bg-blue-600 focus:outline-none focus:ring focus:ring-blue-300">Login</button>
-            {{--
-        </form> --}}
+
+        </form>
     </div>
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,16 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::view('/', 'auth.login')->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+Route::get('/logout', [AuthController::class, 'logout'])->middleware('auth');
 
-Route::view('/', 'auth.login');
+Route::group(["prefix" => "admin", "middleware" => ["auth", "adminCheck"], "as" => "admin."], function () {
+    Route::view('/', 'layouts.layout');
+});
+
+Route::group(["prefix" => "employee", "middleware" => ["auth", "employeeCheck"], "as" => "employee."], function () {
+    Route::view('/', 'layouts.layout');
+});
